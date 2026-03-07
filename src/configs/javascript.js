eaaa153bb89b7865af6c6603277028f9f079cd4d
Eduard Aksamitov <e@euaaaio.ru>
2026-03-07 16:59:31 +0300

    feat(js): disable `no-useless-assignment`

--- src/configs/javascript.js
+++ src/configs/javascript.js
@@ -157,7 +157,6 @@ export function javascript() {
 				'no-unused-labels': 'error',
 				'no-unused-private-class-members': 'error',
 				'no-unused-vars': 'error',
-				'no-useless-assignment': 'error',
 				'no-useless-backreference': 'error',
 				'no-useless-call': 'error',
 				'no-useless-catch': 'error',
